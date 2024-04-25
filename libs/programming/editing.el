88551d81ed720f69e0721a470de5500a13c73e62
Sventimir <svantimir@gmail.com>
2024-04-25 16:27:44 +0200

    Add a function to convert snake case to camel case.

--- libs/programming/editing.el
+++ libs/programming/editing.el
@@ -10,6 +10,13 @@
     (insert (char-to-string (+ char 32)))
     (backward-char)))
 
+(defun upcase-current-char ()
+  "Replace current char with its uppercase version."
+  (let ((char (char-after)))
+    (delete-char 1)
+    (insert (char-to-string (- char 32)))
+    (backward-char)))
+
 (defun transform-camel-until-end-of-word (trans-cap)
   "Convert the word at point using TRANS-CAP."
   (forward-char)
@@ -21,6 +28,20 @@
      ((capital-letter-p char) (progn
                                 (funcall trans-cap nil)
                                 (transform-camel-until-end-of-word trans-cap)))
+     (t nil))))
+
+(defun transform-snake-until-end-of-word (trans-underscore)
+  "Convert the snake-case word at point using TRANS-UNDERSCORE."
+  (forward-char)
+  (let ((char (char-after)))
+    (cond
+     ((or (small-letter-p char) (digit-p char) (capital-letter-p char))
+      (progn
+        (transform-snake-until-end-of-word trans-underscore)))
+     ((equal char ?_)
+      (progn
+        (funcall trans-underscore nil)
+        (transform-snake-until-end-of-word trans-underscore)))
      (t nil))))
 
 (defun cap-to-snake (first-p)
@@ -39,7 +60,20 @@
        ((capital-letter-p c) (progn
                                (funcall trans-cap t)
                                (transform-camel-until-end-of-word trans-cap)))
-       (t (message "tramsform-camel-case only makes sense for words: '%c'" c))))))
+       (t (message "transform-camel-case only makes sense for words: '%c'" c))))))
+
+(defun transform-snake-case (trans-underscore)
+  "Transform the word at point by applying TRANS-UNDERSCORE to every underscore."
+  (save-excursion
+    (forward-char)
+    (forward-word -1)
+    (let ((c (char-after)))
+      (cond
+       ((or (small-letter-p c) (digit-p c)) (transform-snake-until-end-of-word trans-underscore))
+       ((equal c ?_) (progn
+                               (funcall trans-underscore t)
+                               (transform-camel-until-end-of-word trans-underscore)))
+       (t (message "transform-snake-case only makes sense for words: '%c'" c))))))
 
 (defun to-snake-case ()
   "Convert the word at point to snake case."
@@ -52,6 +86,12 @@
   (transform-camel-case (lambda (first-p)
                           (if (not first-p) (insert " ")))))
 
+(defun to-camel-case ()
+  "Convert the word at point to camel case."
+  (interactive)
+  (transform-snake-case (lambda (first-p)
+                          (if (not first-p) (delete-char 1))
+                          (upcase-current-char))))
+
 (provide 'editing)
 ;;; editing.el ends here
-
