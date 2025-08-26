69f94821ca524b5e36dcd491fc1709be169b50a2
Sventimir <svantimir@gmail.com>
2025-08-26 11:18:39 +0200

    Restore ellama support

--- init.el
+++ init.el
@@ -419,7 +419,14 @@
  '(org-sql-db-config
    '(postgres :database "postgres" :hostname "localhost" :username "sven"))
  '(org-support-shift-select 'always)
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(ac-helm company crux csv-mode deferred direnv dockerfile-mode dune
+             elisp-format ellama envrc evil flycheck gnuplot
+             graphql-mode haskell-mode helm-lsp idris-mode json-mode
+             lsp-ui lua-mode magit nix-mode perspective pyvenv
+             quelpa-use-package request rust-mode solidity-mode
+             tree-sitter tuareg typescript-mode undo-tree yaml-mode
+             yasnippet))
  '(python-indent-guess-indent-offset nil)
  '(python-indent-offset 4)
  '(recorder-default-writing-dir "/home/sven/archive/movie/rec")
@@ -527,72 +534,72 @@
                              " "))))
 
 ;; Ellama LLM assistant
-;; (use-package ellama
-;;   :ensure t
-;;   :bind ("C-a" . ellama-transient-main-menu)
-;;   :init
-;;   ;; setup key bindings
-;;   ;; (setopt ellama-keymap-prefix "C-c e")
-;;   ;; language you want ellama to translate to
-;;   (setopt ellama-language "English")
-;;   ;; could be llm-openai for example
-;;   (require 'llm-ollama)
-;;   (setopt ellama-provider
-;;   	  (make-llm-ollama
-;;   	   ;; this model should be pulled to use it
-;;   	   ;; value should be the same as you print in terminal during pull
-;;   	   :chat-model "llama3:8b-instruct-q8_0"
-;;   	   :embedding-model "nomic-embed-text"
-;;   	   :default-chat-non-standard-params '(("num_ctx" . 8192))))
-;;   (setopt ellama-summarization-provider
-;;   	  (make-llm-ollama
-;;   	   :chat-model "qwen2.5:3b"
-;;   	   :embedding-model "nomic-embed-text"
-;;   	   :default-chat-non-standard-params '(("num_ctx" . 32768))))
-;;   (setopt ellama-coding-provider
-;;   	  (make-llm-ollama
-;;   	   :chat-model "qwen2.5-coder:3b"
-;;   	   :embedding-model "nomic-embed-text"
-;;   	   :default-chat-non-standard-params '(("num_ctx" . 32768))))
-;;   ;; Predefined llm providers for interactive switching.
-;;   ;; You shouldn't add ollama providers here - it can be selected interactively
-;;   ;; without it. It is just example.
-;;   (setopt ellama-providers
-;;   	  '(("zephyr" . (make-llm-ollama
-;;   			 :chat-model "zephyr:7b-beta-q6_K"
-;;   			 :embedding-model "zephyr:7b-beta-q6_K"))
-;;   	    ("mistral" . (make-llm-ollama
-;;   			  :chat-model "mistral:7b-instruct-v0.2-q6_K"
-;;   			  :embedding-model "mistral:7b-instruct-v0.2-q6_K"))
-;;   	    ("mixtral" . (make-llm-ollama
-;;   			  :chat-model "mixtral:8x7b-instruct-v0.1-q3_K_M-4k"
-;;   			  :embedding-model "mixtral:8x7b-instruct-v0.1-q3_K_M-4k"))))
-;;   ;; Naming new sessions with llm
-;;   (setopt ellama-naming-provider
-;;   	  (make-llm-ollama
-;;   	   :chat-model "llama3:8b-instruct-q8_0"
-;;   	   :embedding-model "nomic-embed-text"
-;;   	   :default-chat-non-standard-params '(("stop" . ("\n")))))
-;;   (setopt ellama-naming-scheme 'ellama-generate-name-by-llm)
-;;   ;; Translation llm provider
-;;   (setopt ellama-translation-provider
-;;   	  (make-llm-ollama
-;;   	   :chat-model "qwen2.5:3b"
-;;   	   :embedding-model "nomic-embed-text"
-;;   	   :default-chat-non-standard-params
-;;   	   '(("num_ctx" . 32768))))
-;;   (setopt ellama-extraction-provider (make-llm-ollama
-;;   				      :chat-model "qwen2.5-coder:7b-instruct-q8_0"
-;;   				      :embedding-model "nomic-embed-text"
-;;   				      :default-chat-non-standard-params
-;;   				      '(("num_ctx" . 32768))))
-;;   ;; customize display buffer behaviour
-;;   ;; see ~(info "(elisp) Buffer Display Action Functions")~
-;;   (setopt ellama-chat-display-action-function #'display-buffer-full-frame)
-;;   (setopt ellama-instant-display-action-function #'display-buffer-at-bottom)
-;;   :config
-;;   ;; send last message in chat buffer with C-c C-c
-;;   (add-hook 'org-ctrl-c-ctrl-c-hook #'ellama-chat-send-last-message))
+(use-package ellama
+  :ensure t
+  :bind ("C-a" . ellama-transient-main-menu)
+  :init
+  ;; setup key bindings
+  ;; (setopt ellama-keymap-prefix "C-c e")
+  ;; language you want ellama to translate to
+  (setopt ellama-language "English")
+  ;; could be llm-openai for example
+  (require 'llm-ollama)
+  (setopt ellama-provider
+  	  (make-llm-ollama
+  	   ;; this model should be pulled to use it
+  	   ;; value should be the same as you print in terminal during pull
+  	   :chat-model "llama3:8b-instruct-q8_0"
+  	   :embedding-model "nomic-embed-text"
+  	   :default-chat-non-standard-params '(("num_ctx" . 8192))))
+  (setopt ellama-summarization-provider
+  	  (make-llm-ollama
+  	   :chat-model "qwen2.5:3b"
+  	   :embedding-model "nomic-embed-text"
+  	   :default-chat-non-standard-params '(("num_ctx" . 32768))))
+  (setopt ellama-coding-provider
+  	  (make-llm-ollama
+  	   :chat-model "qwen2.5-coder:3b"
+  	   :embedding-model "nomic-embed-text"
+  	   :default-chat-non-standard-params '(("num_ctx" . 32768))))
+  ;; Predefined llm providers for interactive switching.
+  ;; You shouldn't add ollama providers here - it can be selected interactively
+  ;; without it. It is just example.
+  (setopt ellama-providers
+  	  '(("zephyr" . (make-llm-ollama
+  			 :chat-model "zephyr:7b-beta-q6_K"
+  			 :embedding-model "zephyr:7b-beta-q6_K"))
+  	    ("mistral" . (make-llm-ollama
+  			  :chat-model "mistral:7b-instruct-v0.2-q6_K"
+  			  :embedding-model "mistral:7b-instruct-v0.2-q6_K"))
+  	    ("mixtral" . (make-llm-ollama
+  			  :chat-model "mixtral:8x7b-instruct-v0.1-q3_K_M-4k"
+  			  :embedding-model "mixtral:8x7b-instruct-v0.1-q3_K_M-4k"))))
+  ;; Naming new sessions with llm
+  (setopt ellama-naming-provider
+  	  (make-llm-ollama
+  	   :chat-model "llama3:8b-instruct-q8_0"
+  	   :embedding-model "nomic-embed-text"
+  	   :default-chat-non-standard-params '(("stop" . ("\n")))))
+  (setopt ellama-naming-scheme 'ellama-generate-name-by-llm)
+  ;; Translation llm provider
+  (setopt ellama-translation-provider
+  	  (make-llm-ollama
+  	   :chat-model "qwen2.5:3b"
+  	   :embedding-model "nomic-embed-text"
+  	   :default-chat-non-standard-params
+  	   '(("num_ctx" . 32768))))
+  (setopt ellama-extraction-provider (make-llm-ollama
+  				      :chat-model "qwen2.5-coder:7b-instruct-q8_0"
+  				      :embedding-model "nomic-embed-text"
+  				      :default-chat-non-standard-params
+  				      '(("num_ctx" . 32768))))
+  ;; customize display buffer behaviour
+  ;; see ~(info "(elisp) Buffer Display Action Functions")~
+  (setopt ellama-chat-display-action-function #'display-buffer-full-frame)
+  (setopt ellama-instant-display-action-function #'display-buffer-at-bottom)
+  :config
+  ;; send last message in chat buffer with C-c C-c
+  (add-hook 'org-ctrl-c-ctrl-c-hook #'ellama-chat-send-last-message))
 
 ;; Spell-checking
 (add-hook 'text-mode-hook 'flyspell-mode)
